7f2535ddf8862470c56bce459de4166bc997c9d0
Sophia Robert <sophia.r.robert@gmail.com>
2020-09-26 07:35:45 +0000

    round -> Math.round

--- configural_VS_featural.js
+++ configural_VS_featural.js
@@ -424,7 +424,7 @@ function block_instructionRoutineBegin(snapshot) {
         }
     }
     trial_order = [[shuffle([1, 2, 3, 4, 5, 6]), shuffle([1, 2, 3, 4, 5, 6]), shuffle([1, 2, 3, 4, 5, 6]), shuffle([1, 2, 3, 4, 5, 6])]];
-    trial_order = round(((trial_order / 6) - 0.1));
+    trial_order = Math.round(((trial_order / 6) - 0.1));
     trialSame = [[shuffle([0, 1, 2, 3]), shuffle([0, 1, 2, 3]), shuffle([0, 1, 2, 3])]];
     trialDiff = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [1, 0], [2, 3], [2, 0], [2, 1], [3, 0], [3, 1], [3, 2]];
     diffTrial = list(range(12));
