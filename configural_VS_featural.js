9c0532c164deb2a1981cd9c4eafc45f0c3a674c9
Sophia Robert <sophia.r.robert@gmail.com>
2020-09-26 06:10:21 +0000

    remove os

--- configural_VS_featural.js
+++ configural_VS_featural.js
@@ -84,6 +84,58 @@ function updateInfo() {
   return Scheduler.Event.NEXT;
 }
 
+var face_config_paths, face_feat_paths, haus_config_paths, haus_feat_paths, stim_config_dir, stim_feat_dir;
+stim_feat_dir = "stimuli/Featural_Set/";
+stim_config_dir = "stimuli/Spacing_Set/";
+face_feat_paths = function () {
+    var _pj_a = [], _pj_b = os.listdir(stim_feat_dir);
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+        var file = _pj_b[_pj_c];
+        if (file.endswith(".tif")) {
+            _pj_a.push(((stim_feat_dir + "/") + file));
+        }
+    }
+    return _pj_a;
+}
+.call(this);
+face_feat_paths.sort();
+face_config_paths = function () {
+    var _pj_a = [], _pj_b = stim_config_dir;
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+        var file = _pj_b[_pj_c];
+        if (file.endswith(".tif")) {
+            _pj_a.push(((stim_config_dir + "/") + file));
+        }
+    }
+    return _pj_a;
+}
+.call(this);
+face_config_paths.sort();
+haus_feat_paths = function () {
+    var _pj_a = [], _pj_b = stim_feat_dir;
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+        var file = _pj_b[_pj_c];
+        if (file.endswith(".bmp")) {
+            _pj_a.push(((stim_feat_dir + "/") + file));
+        }
+    }
+    return _pj_a;
+}
+.call(this);
+haus_feat_paths.sort();
+haus_config_paths = function () {
+    var _pj_a = [], _pj_b = stim_config_dir;
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+        var file = _pj_b[_pj_c];
+        if (file.endswith(".bmp")) {
+            _pj_a.push(((stim_config_dir + "/") + file));
+        }
+    }
+    return _pj_a;
+}
+.call(this);
+haus_config_paths.sort();
+
 var exp_code_setupClock;
 var block_instructionClock;
 var instructions;
